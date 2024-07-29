44d298eed5f3894fd85f2b4e103c38cf77d34207
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-29 11:37:12 -0600

    primer blog

--- src/app/blog/java/tiposVariables/page.js
+++ src/app/blog/java/tiposVariables/page.js
@@ -1,4 +1,19 @@
 
+export const metadata = {
+  title: "Tipos de Variables en Java - Geek Code",
+  description: "Este es un blog acerca de los tipos de variables que hay en Java",
+  keywords: "tipos de variables java, variables java, java variables, tipos variables java, variables en java, tipos de datos java, tipos de variables en java, tipos de datos en java, cuales variables hay en java , que variables hay en java ",
+  author: "Geek Code",
+
+  openGraph: {
+    type: 'article',
+    url: 'https://geekcode.blog/blog/java/tiposVariables',
+    title: 'Tipos de Variables en Java - Geek Code',
+    description: 'Este es un blog acerca de los tipos de variables que hay en Java',
+   
+  },
+  
+};
 
 function Blog(){
 
