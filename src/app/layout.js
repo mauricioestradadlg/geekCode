8bb39a04b0075728c3c264b9b8712501d956ab2c
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-29 15:02:35 -0600

    ##

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,31 @@
-// src/app/layout.js
-import './globals.css';
-import Header from './componentes/Header';
-import Footer from './componentes/Footer';
-import Head from 'next/head'; // Importa el componente Head
+import "./globals.css";
+import Header from "./componentes/Header";
+import Footer from "./componentes/Footer";
+import Script from 'next/script';
 
 export const metadata = {
   title: "Geek Code",
-  description: "Página web oficial de Geek Code",
+  description: "Pagina web oficial de Geek Code",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <Head>
-        {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YYD70KBY3L"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-YYD70KBY3L');
-            `,
-          }}
-        />
-      </Head>
+    <html lang="es">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YYD70KBY3L"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YYD70KBY3L');
+          `}
+        </Script>
+      </head>
       <body>
-        <Header/>
+        <Header />
         <main>{children}</main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
