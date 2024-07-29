e8105a88cc76543db712172f21c3d84dc1f9c105
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-29 14:10:24 -0600

    google analitycs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,35 @@
-
-import "./globals.css";
-import Header from "./componentes/Header";
-import Footer from "./componentes/Footer";
-
-
-
+// src/app/layout.js
+import './globals.css';
+import Header from './componentes/Header';
+import Footer from './componentes/Footer';
+import Head from 'next/head'; // Importa el componente Head
 
 export const metadata = {
   title: "Geek Code",
-  description: "Pagina web oficial de Geek Code",
+  description: "Página web oficial de Geek Code",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YYD70KBY3L"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-YYD70KBY3L');
+            `,
+          }}
+        />
+      </Head>
       <body>
-      <Header/>
+        <Header/>
         <main>{children}</main>
-      <Footer/>
+        <Footer/>
       </body>
     </html>
   );
