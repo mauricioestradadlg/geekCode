9725299063b107bb482063e868f54a5f0cc93921
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-29 14:24:00 -0600

    sitemap robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://geekcode.blog/', // Cambia esto por la URL de tu sitio
+    generateRobotsTxt: true, // (Opcional) Genera el archivo robots.txt
+    // Puedes agregar más opciones aquí según tus necesidades
+  };
+  
