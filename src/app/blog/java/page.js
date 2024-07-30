2b8591d0b98ea35c7213ee5745b5e17d83da74c0
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-30 14:07:57 -0600

    java blogs

--- src/app/blog/java/page.js
+++ src/app/blog/java/page.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import java2 from '../imagenes/javaBlogs.jpg';
+import img1 from '../imagenes/img1.png';
 import styles from '../page.module.css'
 
 function Blog(){
@@ -14,6 +15,8 @@ function Blog(){
     <br/><br/>
 
     <Link href='/blog/java/tiposVariables'><h2>Tipos de Variables de Java</h2></Link>
+    <Image src={img1} className={styles.imgInicio} alt ="logo"  width={900} height={500}/>
+    <br/>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
       sed do eiusmod tempor incididunt ut labore et dolore magna 
       aliqua. Ut enim ad minim veniam, quis nostrud exercitation
