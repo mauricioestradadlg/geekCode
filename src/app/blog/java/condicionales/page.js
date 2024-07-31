6ef0fd823a67bf1dd08cf317628170cf46c0d93b
mauricioestradadlg <mauricioestrada580@gmail.com>
2024-07-31 10:59:24 -0600

    blog corregido

--- src/app/blog/java/condicionales/page.js
+++ src/app/blog/java/condicionales/page.js
@@ -33,47 +33,43 @@ export const metadata = {
   canonical: 'https://geekcode.blog/blog/java/condicionales',
 };
 
-function Blog(){
+function Blog() {
   return (
     <>
       <h1 id={styles.h1Inicio}>TIPOS DE CONDICIONALES EN JAVA</h1>
 
       <Image src={img1} className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
 
-
       <h2 id={styles.h2Blog}>Introducción</h2>
       <p id={styles.pBlog}>En el desarrollo de software, la capacidad de tomar decisiones basadas en condiciones es fundamental. Java, como lenguaje de programación orientado a objetos, ofrece una variedad de estructuras condicionales que permiten a los desarrolladores controlar el flujo de ejecución de sus programas. En este artículo, exploraremos los diferentes tipos de condicionales en Java y cómo se utilizan.</p>
       <br/>
       <Image src={java3} className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
       <br/><br/>
-      
 
-      <h2 id={styles.h2Blog}>1. Condicion 'IF'</h2>
+      <h2 id={styles.h2Blog}>1. Condicion &apos;IF&apos;</h2>
       <p id={styles.pBlog}>El condicional if es la estructura básica de control de flujo en Java. Se utiliza para ejecutar un bloque de código solo si una condición especificada es true.</p>
       <Image src={img2} className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
       <br/><br/>
 
-      <h2 id={styles.h2Blog}>2. Condicion 'ELSE'</h2>
+      <h2 id={styles.h2Blog}>2. Condicion &apos;ELSE&apos;</h2>
       <p id={styles.pBlog}>La declaración else se utiliza junto con if para ejecutar un bloque de código alternativo si la condición en el if es false.</p>
-      <Image src={img3}  className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
+      <Image src={img3} className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
       <br/><br/>
 
-      <h2 id={styles.h2Blog}>3. Condicion 'ELSE-IF'</h2>
+      <h2 id={styles.h2Blog}>3. Condicion &apos;ELSE-IF&apos;</h2>
       <p id={styles.pBlog}>Cuando se tienen múltiples condiciones que deben evaluarse, se puede usar la declaración else-if. Esta estructura permite manejar múltiples alternativas de manera ordenada.</p>
       <Image src={img4} className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
       <br/><br/>
 
-      <h2 id={styles.h2Blog}>4. Condicion 'SWITCH'</h2>
+      <h2 id={styles.h2Blog}>4. Condicion &apos;SWITCH&apos;</h2>
       <p id={styles.pBlog}>La declaración switch es una alternativa al uso de múltiples if-else-if cuando se trabaja con valores que pueden tomar un número limitado de opciones. Es especialmente útil cuando se necesita comparar una variable con varios valores específicos.</p>
-      <Image src={img5}  className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
+      <Image src={img5} className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
       <br/><br/>
 
       <h2 id={styles.h2Blog}>5. Operador Ternario</h2>
       <p id={styles.pBlog}>El operador ternario es una forma compacta de escribir una condición if-else. Es ideal para situaciones donde se necesita asignar un valor basado en una condición en una sola línea.</p>
-      <Image src={img6}  className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
+      <Image src={img6} className={styles.imgInicio} alt="Java Variables" width={900} height={500} />
       <br/><br/>
-
-      
     </>
   );
 }
